93f356798745bd278058d362298c587b3f2071d8
hipertracker <hipertracker@gmail.com>
2017-12-04 04:20:12 +0000

    add comment about debugger

--- src/controller.ts
+++ src/controller.ts
@@ -2,6 +2,7 @@ import { Controller } from '@cerebral/mobx-state-tree'
 
 import app from './modules/app'
 
+// see: https://cerebraljs.com/docs/introduction/debugger.html
 const Devtools = (
   process.env.NODE_ENV === 'development'
     ? require('cerebral/devtools').default
